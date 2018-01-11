e24aaed47d6125001a0a6a83cd92869ef21519d8
udarange <udarange@gmail.com>
2018-01-11 17:36:47 +0530

    Change App.java file. Add 0 selection for exit

--- modules/cal-core/src/main/java/com/hsm/App.java
+++ modules/cal-core/src/main/java/com/hsm/App.java
@@ -4,41 +4,47 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        double num1 = 0;
-        double num2 = 0;
-        double ans;
-        System.out.println("Enter your selection: \n 1 for Addition \n 2 for substraction \n 3 for Multiplication \n 4 for division");
-        Scanner sc = new Scanner(System.in);
-        int selection;
-        selection = sc.nextInt();
+        while (true) {
+            double num1;
+            double num2;
+            double ans;
 
-        if (selection < 5) {
-            System.out.println("Enter first number :");
-            num1 = sc.nextDouble();
-            System.out.println("Enter second number :");
-            num2 = sc.nextDouble();
-            System.out.print("Answer : ");
-        }
+            System.out.println("Enter your selection: \n 1 for Addition \n 2 for substraction \n 3 for Multiplication \n 4 for division\n 0 for EXIT");
+            Scanner sc = new Scanner(System.in);
+            int selection;
+            selection = sc.nextInt();
 
-        switch (selection) {
-            case 1:
-                ans = num1 + num2;
-                System.out.printf("%.2f\n", ans);
+            if (selection < 5 && selection > 0) {
+                System.out.println("Enter first number :");
+                num1 = sc.nextDouble();
+                System.out.println("Enter second number :");
+                num2 = sc.nextDouble();
+                System.out.print("Answer : ");
+
+                switch (selection) {
+                    case 1:
+                        ans = num1 + num2;
+                        System.out.printf("%.2f\n", ans);
+                        break;
+                    case 2:
+                        ans = num1 - num2;
+                        System.out.printf("%.2f\n", ans);
+                        break;
+                    case 3:
+                        ans = num1 * num2;
+                        System.out.printf("%.2f\n", ans);
+                        break;
+                    case 4:
+                        ans = num1 / num2;
+                        System.out.printf("%.2f\n", ans);
+                        break;
+                }
                 break;
-            case 2:
-                ans = num1 - num2;
-                System.out.printf("%.2f\n", ans);
+            } else if (selection == 0) {
                 break;
-            case 3:
-                ans = num1 * num2;
-                System.out.printf("%.2f\n", ans);
-                break;
-            case 4:
-                ans = num1 / num2;
-                System.out.printf("%.2f\n", ans);
-                break;
-            default:
-                System.out.println("Illigal Operation");
+            } else {
+                System.out.println("Illigal Operation. Try again or press 0 for exit");
+            }
         }
     }
 }
